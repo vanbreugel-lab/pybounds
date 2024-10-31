841953fefbbd8e94358c9a1ef606324207249f73
BC <bcellini00@gmail.com>
2024-10-31 15:40:49 -0700

    Update version

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pybounds", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Ben Cellini, Burak Boyacioglu, Floris van Breugel",
     author_email="bcellini00@gmail.com",
     description="Bounding Observability for Uncertain Nonlinear Dynamics Systems (BOUNDS)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/vanbreugel-lab/pybounds",
+    url="https://pypi.org/project/pybounds/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
